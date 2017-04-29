280c3f425e2fb1e61234134b4b17ea898dea357b
Zhao Yulang <yulang.zhao@ge.com>
2017-04-29 21:41:08 +0800

    Hello, Yulang! with template

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = { 'nickname': 'Yulang' }
+    return render_template('index.html',
+            title = 'Home',
+            user = user)
 
