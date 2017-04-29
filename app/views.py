58b90f8dfc27c5d239ab7443b6803d15ec13be06
Zhao Yulang <yulang.zhao@ge.com>
2017-04-29 22:19:41 +0800

    Loops in Template

--- app/views.py
+++ app/views.py
@@ -6,6 +6,18 @@ from app import app
 @app.route('/index')
 def index():
     user = { 'nickname': 'Yulang' }
+    posts = [
+            {
+                'author': { 'nickname': 'John' },
+                'body': 'Beautiful day in Portland!'
+            },
+            {
+                'author': { 'nickname': 'Susan' },
+                'body': 'The Avengers movie was so cool!'
+            }
+            ]
     return render_template('index.html',
-            user = user)
+            title = 'Home',
+            user = user,
+            posts = posts)
 
