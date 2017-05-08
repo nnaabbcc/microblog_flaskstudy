fff5d49fabb1771e41abea0e5a301e9e8921bb28
nnaabbcc <nnaabbcc.zhao@gmail.com>
2017-05-08 21:39:05 +0800

    script for database upgrade

---
+++ db_upgrade.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+
+api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+print('Current database version: ' + str(v))
