d4f63c2e241851b69a0cc93dbfee484532f04473
nnaabbcc <nnaabbcc.zhao@gmail.com>
2017-05-08 21:32:50 +0800

    configure database

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 
-CSRF_ENABLED = True
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# Remove warning 'SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be 
+# disabled by default in the future.  Set it to True to suppress this 
+# warning.'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+CSRF_ENABLED = True
 SECRET_KEY = '48b0ed7b-f1cb-47cd-be30-b8a560421c6f'
 
 OPENID_PROVIDERS = [
